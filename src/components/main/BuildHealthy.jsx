fe2917da72c1e715ec00cfc0283cdc50e7f69a6b
Nikita O'Keeffe <okeeffe.nikita@gmail.com>
2024-06-20 19:36:12 +1000

    moar mobile formatting :3

--- src/components/main/BuildHealthy.jsx
+++ src/components/main/BuildHealthy.jsx
@@ -1,6 +1,6 @@
 function BuildHealthy() {
   return (
-    <section className="mx-auto flex h-[30svh] w-full select-none flex-col items-center justify-center antialiased sm:h-[35vh]">
+    <section className="mx-auto flex h-[22.5svh] w-full select-none flex-col items-center justify-center antialiased sm:h-[35vh]">
       <div className="mt-2 flex w-full flex-col items-center gap-8 text-center sm:mt-0 sm:w-1/2">
         <h1 className="text-2xl font-bold tracking-tight sm:text-5xl">
           Build healthy habits, <br />
