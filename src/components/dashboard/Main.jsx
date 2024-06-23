09c30c8e986deed66cff28abedee249e86531393
Nikita O'Keeffe <okeeffe.nikita@gmail.com>
2024-06-23 13:19:55 +1000

    .

--- src/components/dashboard/Main.jsx
+++ src/components/dashboard/Main.jsx
@@ -1,7 +1,9 @@
 function Main({ user }) {
   return (
-    <div className="flex flex-grow mt-4 px-4 py-4 w-full flex-col bg-gray-100 max-w-[1600px] mx-auto ">
-      <h1 className="text-xl font-semibold tracking-tight text-black/85">Welcome, {user.displayName}!</h1>
+    <div className="mx-auto mt-4 flex w-full max-w-[1600px] flex-grow flex-col bg-gray-100 px-4 py-4">
+      <h1 className="text-xl font-semibold tracking-tight text-black/85">
+        Welcome, {user.displayName}!
+      </h1>
     </div>
   );
 }
